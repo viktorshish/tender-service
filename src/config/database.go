57910b3858f163c1a97073111632c273c50414fc
Shishkin PRO <viktorshish@gmail.com>
2024-09-12 10:04:24 +0300

    fix: apply missing changes for db connection

--- src/config/database.go
+++ src/config/database.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dbUsername := os.Getenv("POSTGRES_USERNAME")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbHost := os.Getenv("POSTGRES_HOST")
+	dbPort := os.Getenv("POSTGRES_PORT")
+	dbName := os.Getenv("POSTGRES_DATABASE")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbHost, dbUsername, dbPassword, dbName, dbPort)
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to the database: ", err)
+	}
+	fmt.Println("Database connected successfully")
+}

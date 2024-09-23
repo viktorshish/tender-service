211bdf2862c5f91ed22fb84671c47a268a9782b2
Shishkin PRO <viktorshish@gmail.com>
2024-09-23 23:01:42 +0300

    models: fix employee

--- internal/models/employee.go
+++ internal/models/employee.go
@@ -1,16 +1,17 @@
 package models
 
 import (
+	"github.com/google/uuid"
 	"time"
 )
 
 type Employee struct {
-	ID        string    `gorm:"type:uuid;primary_key" json:"id"`
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
 	Username  string    `gorm:"type:varchar(50);unique;not null" json:"username"`
 	Firstname string    `gorm:"type:varchar(50)" json:"first_name"`
 	Lastname  string    `gorm:"type:varchar(50)" json:"last_name"`
-	CreatedAt time.Time `gorm:"type:timestamp;default:current_timestamp" json:"created_at"`
-	UpdatedAt time.Time `gorm:"type:timestamp;default:current_timestamp" json:"updated_at"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 // Явное указание имени таблицы
